0768d4b480f1e4a4337a08af520bacaee0eeaeab
martantong <martantong@gmail.com>
2021-05-07 20:18:08 +0300

    test_guest_can_go_to_login_page added

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help="Choose language")
+
+
+@pytest.fixture(scope="function")
+def language(request):
+    return request.config.getoption("language")
+
+
+@pytest.fixture(scope="function")
+def browser():
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(3)
+    yield browser
+    browser.quit()
