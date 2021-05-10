4b4eb0e470807ab79107082db58b23d9d3e52986
martantong <79417840+martantong@users.noreply.github.com>
2021-05-10 08:20:29 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -5,7 +5,7 @@ import time
 import pytest
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0")
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"])
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
